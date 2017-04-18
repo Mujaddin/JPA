cadcb1165d96b060cd098b7c0d44c3ae6676b4ec
trainee <trainee>
2017-04-18 11:03:02 +0700

    JPA PROJECT DAY 2

---
+++ src/main/java/java/mitrais/JavaPresistentApplication.java
@@ -0,0 +1,12 @@
+package java.mitrais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPresistentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPresistentApplication.class, args);
+	}
+}
---
+++ src/test/java/java/mitrais/JavaPresistentApplicationTests.java
@@ -0,0 +1,16 @@
+package java.mitrais;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaPresistentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
